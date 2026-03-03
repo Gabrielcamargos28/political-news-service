8d757ac5910ec7da6e8d4c45da4672133c5dc68b
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2026-03-03 11:38:34 -0300

    first commit

---
+++ src/main/java/com/political_news_service/political_news_service/PoliticalNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.political_news_service.political_news_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalNewsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/political_news_service/political_news_service/PoliticalNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.political_news_service.political_news_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalNewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
